62f75edb00b9083ef026b819d3a82f31c0229ada
burrahey <burra.archana@gmail.com>
2018-01-14 12:31:36 -0500

    added containers

--- client/src/containers/Game.js
+++ client/src/containers/Game.js
@@ -1,1 +1,12 @@
 import React, { Component } from 'react';
+
+export class Game extends Component {
+
+  render() {
+    return (
+      <div className="App">
+      Game here
+      </div>
+    );
+  }
+}
--- client/src/containers/Scoreboard.js
+++ client/src/containers/Scoreboard.js
@@ -1,1 +1,12 @@
 import React, { Component } from 'react';
+
+export class Scoreboard extends Component {
+
+  render() {
+    return (
+      <div className="App">
+      Scoreboard
+      </div>
+    );
+  }
+}
