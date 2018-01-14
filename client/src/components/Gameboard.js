b6dbcbbdfaef9ec797637c9fcfabed204e22ed3f
burrahey <burra.archana@gmail.com>
2018-01-14 17:06:59 -0500

    made gameboard into its own class

--- client/src/components/Gameboard.js
+++ client/src/components/Gameboard.js
@@ -1,21 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Cell from './Cell';
 
-const Gameboard = (props) => {
-  const renderedGame = props.gameState.map(row => {return (
-    <tr>
-      {row.map(cell => { return (
-        <td>
-          {cell}
-        </td>
-      )})}
-    </tr>
-  )})
-  return (
-    <table>
-      {renderedGame}
-    </table>
-  )
+export class Gameboard extends Component {
+  render(){
+    const renderedGame = this.props.gameState.map(row => {return (
+      <tr>
+        {row.map(cell => { return (
+          <td>
+            {cell}
+          </td>
+        )})}
+      </tr>
+    )});
+    return(
+      <table>
+        {renderedGame}
+      </table>
+    )
+  }
 }
-
-export default Gameboard;
--- client/src/containers/Game.js
+++ client/src/containers/Game.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import Players from '../components/Players'
-import Gameboard from '../components/Gameboard'
+import {Gameboard} from '../components/Gameboard'
 
 class staticGame extends Component {
 
