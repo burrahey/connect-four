6d9a1a4b51b6052899634d57d22cc1ddfb18f819
burrahey <burra.archana@gmail.com>
2018-01-14 12:59:55 -0500

    added and commented out state info in Intro

--- client/src/containers/Intro.js
+++ client/src/containers/Intro.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 export class Intro extends Component {
+
+  constructor(){
+    super()
+
+    this.state={
+      player1:"", player2:""
+    }
+  }
+
+  handleOnChange =(e) => {
+  }
+
+  handleOnSubmit =(event) => {
+    event.preventDefault();
+  }
 
   render() {
     return (
       <div>
-      Intro
+      <h1>Welcome to Connect Four.</h1>
+      <p>Enter player names to get started.</p>
+      <form>
+      <label>Player 1 Name</label>
+      <input name="player1" type="text" /><br />
+      <label>Player 2 Name</label>
+      <input name="player2" type="text" /><br />
+      <input type="submit" />
+      </form>
       </div>
     );
   }
 }
+
+// const mapStateToProps = (state) => {
+//   return {player1: state.player1, player2: state.player1};
+// }
+//
+// export const Intro = connect(mapStateToProps)(Intro);
