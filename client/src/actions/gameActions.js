0fa5d610ee3985b84e5e818dbd0b9b8793aac5e3
burrahey <burra.archana@gmail.com>
2018-01-15 12:13:20 -0500

    fixed fetch request after game finishes

--- client/src/actions/gameActions.js
+++ client/src/actions/gameActions.js
@@ -9,20 +9,14 @@ export const updateBoard = (column, player) => {
 }
 
 export const saveGame = (winner, gameState) => {
-  debugger;
   fetch('/api/games', {
-  method: "post",
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-    'X-Requested-With': 'XMLHttpRequest'
-  },
-  body: JSON.stringify({ winner: winner, state: gameState })
+    method: "post",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'X-Requested-With': 'XMLHttpRequest'
+    },
+    body: JSON.stringify({ winner: winner, state: gameState })
   })
-  .then( (response) => {
-  console.log(response)
-  debugger;
-});
-
   return {type: "SAVE_GAME"}
 }
