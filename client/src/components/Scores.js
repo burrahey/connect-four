e5610d1dcd3a43ecfea2aa2521e359cfeb79d360
burrahey <burra.archana@gmail.com>
2018-01-15 14:15:20 -0500

    centered table

--- client/src/components/Scores.js
+++ client/src/components/Scores.js
@@ -5,7 +5,7 @@ const Scores = (props) => {
 
   let renderedScores = "";
   if(props.scores.length > 0){
-    renderedScores = props.scores.map((score) => {return <li id={score.id} key={score.id}> Players: {score.player1} and {score.player2}.<br /> Winner: {score.winner} <br /> Date Played: {Moment.utc(props.created_at).format('MMM D, YYYY')} <br /> <button id={score.id} onClick={(e) => {props.handleOnClick(e)}}>See Game</button></li>})
+    renderedScores = props.scores.map((score) => {return <li id={score.id} key={score.id}> <strong>Players:</strong> {score.player1} and {score.player2}<br /> <strong>Winner:</strong> {score.winner} <br /> <strong>Date Played:</strong>  {Moment.utc(props.created_at).format('MMM D, YYYY')} <br /> <button id={score.id} onClick={(e) => {props.handleOnClick(e)}}>See Game</button></li>})
   } else {
     renderedScores = "No Scores Yet"
   }
