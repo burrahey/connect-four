785afca09aa939b92cf500563b38659e7c4dc09d
burrahey <burra.archana@gmail.com>
2018-01-15 14:33:42 -0500

    got the pretty balls

--- client/src/components/Cell.js
+++ client/src/components/Cell.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Cell = (props) => {
+  const position = "circle-" + props.value;
   return (
-    <td onClick={(e) => {props.handleOnClick(e)}} id={props.column.toString()}>
-      {props.value}
+    <td>
+      <div onClick={(e) => {props.handleOnClick(e)}} id={props.column.toString()} className={position}>
+      </div>
     </td>
   )
 }
