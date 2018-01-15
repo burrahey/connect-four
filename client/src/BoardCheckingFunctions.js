493a70a58150d8546538e8a0353e5d49aee94ad1
burrahey <burra.archana@gmail.com>
2018-01-15 06:41:10 -0500

    moved board checking functions out of reducer file

---
+++ client/src/BoardCheckingFunctions.js
@@ -0,0 +1,82 @@
+import React from 'react';
+
+const maxLength = 6;
+
+const checkWinner = function(gameState, row, column, player){
+
+  //checking horizontals
+  for(let i = 0; i < 4; i++){
+    if(gameState[row][column + i - 3] === gameState[row][column + i - 2] && gameState[row][column + i - 2] === gameState[row][column + i - 1] && gameState[row][column + i - 1] === gameState[row][column + i - 0]){
+      console.log("win horizontal row: " + row)
+      return true;
+    }
+  }
+
+  //checking diagonals
+  for(let i = 0; i < 4; i++){
+    if(((row + i - 3) >= 0) && ((row + i) < maxLength)){
+      //left to right
+      if(gameState[row + i][column - i] === gameState[row + i - 1][column - i + 1] && gameState[row + i - 1][column - i + 1] === gameState[row + i - 2][column -i + 2] && gameState[row + i - 2][column - i + 2] === gameState[row + i - 3][column - i+ 3]){
+        console.log("diagonal win left to right row:" + row + " column: " + column + " i: " + i)
+        return true;
+      } else if(gameState[row + i][column + i] === gameState[row + i - 1][column + i - 1] && gameState[row + i - 1][column + i - 1] === gameState[row + i - 2][column + i - 2] && gameState[row + i - 2][column + i - 2] === gameState[row + i - 3][column + i - 3]){
+        //right to left
+        console.log("diagonal win right to left row:" + row + " column: " + column + " i: " + i)
+        return true;
+      }
+    }
+  }
+
+  // checking vertical wins
+  if(row < 3){
+   if(gameState[row][column] === gameState[row+1][column] && gameState[row+1][column] === gameState[row+2][column] && gameState[row+2][column] === gameState[row+3][column]){
+     console.log("win vertical: " + column)
+     return true
+   }
+ }
+
+  return false;
+}
+
+const checkTie = function(gameState){
+  //check to see if the top row is filled
+  if(gameState[0].reduce((a, b) => a + b, 0) > 6){
+    console.log("tie")
+    return true
+  } else {
+    return false
+  }
+}
+
+const updateTurn = function(turn){
+  if(turn === 1){
+    return 2;
+  } else {
+    return 1;
+  }
+}
+
+const updateTurnAndCheckBoard = function(state, action){
+  let newGameState = [...state.gameState];
+  let column = +action.column;
+  let newTurn = state.turn
+  let gameOverValue = false;
+
+  for(let row = 5; row >= 0; row--){
+    if(newGameState[row][column] === 0){
+      newGameState[row][column] = action.player;
+      if(checkWinner(newGameState, row, column, action.player)){
+        gameOverValue = action.player;
+      } else if(checkTie(newGameState)){
+        gameOverValue = "Tie Game";
+      } else {
+        newTurn = updateTurn(state.turn)
+      }
+      break;
+    }
+  }
+
+  return Object.assign({},state, {gameState: newGameState}, {turn: newTurn}, {gameOver: gameOverValue})
+}
+
+export default updateTurnAndCheckBoard;
--- client/src/reducers/game_reducer.js
+++ client/src/reducers/game_reducer.js
@@ -1,3 +1,7 @@
+import React, { Component } from 'react';
+import updateTurnAndCheckBoard from '../BoardCheckingFunctions';
+
+
 const defaultGame = [
   [0,0,0,0,0,0,0],
   [0,0,0,0,0,0,0],
@@ -7,90 +11,7 @@ const defaultGame = [
   [0,0,0,0,0,0,0]
 ];
 
-const maxRowLength = 7;
-
-const checkWinner = function(gameState, row, column, player){
-  let winSum = player * 4;
-  let maxLength = 6;
-
-  //checking horizontals
-  for(let i = 0; i < 4; i++){
-    if(gameState[row][column + i - 3] === gameState[row][column + i - 2] && gameState[row][column + i - 2] === gameState[row][column + i - 1] && gameState[row][column + i - 1] === gameState[row][column + i - 0]){
-      console.log("win horizontal row: " + row)
-      return true;
-    }
-  }
-
-  //checking diagonals
-  for(let i = 0; i < 4; i++){
-    if(((row + i - 3) >= 0) && ((row + i) < maxLength)){
-      //left to right
-      console.log("checking row:" + row + " column: " + column + " i: " + i)
-      if(gameState[row + i][column - i] === gameState[row + i - 1][column - i + 1] && gameState[row + i - 1][column - i + 1] === gameState[row + i - 2][column -i + 2] && gameState[row + i - 2][column - i + 2] === gameState[row + i - 3][column - i+ 3]){
-        console.log("diagonal win left to right row:" + row + " column: " + column + " i: " + i)
-        return true;
-      } else if(gameState[row + i][column + i] === gameState[row + i - 1][column + i - 1] && gameState[row + i - 1][column + i - 1] === gameState[row + i - 2][column + i - 2] && gameState[row + i - 2][column + i - 2] === gameState[row + i - 3][column + i - 3]){
-        //right to left
-        console.log("diagonal win right to left row:" + row + " column: " + column + " i: " + i)
-        return true;
-
-      }
-    }
-  }
-
-  // checking vertical wins
-  if(row < 3){
-   if(gameState[row][column] === gameState[row+1][column] && gameState[row+1][column] === gameState[row+2][column] && gameState[row+2][column] === gameState[row+3][column]){
-     console.log("win vertical: " + column)
-     return true
-   }
- }
-
-  return false;
-}
-
-const checkTie = function(gameState){
-  //check to see if the top row is filled
-  if(gameState[0].reduce((a, b) => a + b, 0) > 6){
-    console.log("tie")
-    return true
-  } else {
-    return false
-  }
-}
-
-const updateTurn = function(turn){
-  if(turn === 1){
-    return 2;
-  } else {
-    return 1;
-  }
-}
-
-const updateTurnAndCheckBoard = function(state, action){
-  let newGameState = [...state.gameState];
-  let column = +action.column;
-  let newTurn = state.turn
-  let gameOverValue = false;
-
-  for(let row = 5; row >= 0; row--){
-    if(newGameState[row][column] === 0){
-      newGameState[row][column] = action.player;
-      if(checkWinner(newGameState, row, column, action.player)){
-        gameOverValue = action.player;
-      } else if(checkTie(newGameState)){
-        gameOverValue = "Tie Game";
-      } else {
-        newTurn = updateTurn(state.turn)
-      }
-      break;
-    }
-  }
-
-  return Object.assign({},state, {gameState: newGameState}, {turn: newTurn}, {gameOver: gameOverValue})
-}
-
-const gameReducer = function(state={player1: "Archana", player2: "Billu", gameState: defaultGame, turn: 1, gameOver: false}, action){
+const gameReducer = function(state={player1: "Player 1", player2: "Player 2", gameState: defaultGame, turn: 1, gameOver: false}, action){
   switch(action.type){
     case 'ADD_PLAYERS':
       return Object.assign({}, state, action.players);
